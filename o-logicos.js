82517b747d622b0722203372bc1e6fbe28c80e26
Javier Gonzalez <javgon2306@gmail.com>
2024-07-26 12:37:00 -0300

    and, or, not

--- o-logicos.js
+++ o-logicos.js
@@ -8,7 +8,13 @@ let mayor = true;
 let suscripcion = true;
 let saldo = true;
 
-console.log(mayor && suscripcion && saldo);
+console.log('Operador AND', mayor && suscripcion && saldo);
 
 // OR
 
+console.log('Operaodor OR', mayor || suscripcion || saldo);
+
+// NOT
+
+console.log('Operador NOT', !mayor);
+let categoriaInfantil = !mayor;
