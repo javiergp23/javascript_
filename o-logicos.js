5a5a5a0e6ce773dc671dcc58b31ed25ec88ebc8a
Javier Gonzalez <javgon2306@gmail.com>
2024-07-26 12:29:52 -0300

    first commit

---
+++ o-logicos.js
@@ -0,0 +1,14 @@
+// AND OR NOT
+
+console.log(true && true );
+console.log(true && false);
+console.log(false && false );
+
+let mayor = true;
+let suscripcion = true;
+let saldo = true;
+
+console.log(mayor && suscripcion && saldo);
+
+// OR
+
