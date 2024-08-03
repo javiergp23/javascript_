2e446ce816fdc2a34f90950fda7c3e655cb47b8d
Javier Gonzalez <javgon2306@gmail.com>
2024-08-03 00:35:45 -0300

    iteradores

---
+++ ProyectoCarrito/app.js
@@ -0,0 +1,18 @@
+// Variables
+const carrito = document.getElementById("#carrito");
+const contenedorCarrito = document.getElementById("#lista-carrito tbody");
+const vaciarCarrito = document.getElementById("#vaciar-carrito");
+const listaCursos = document.getElementById('#lista-cursos');
+
+cargarEventListeners();
+function cargarEventListeners(){
+    // Cuando agregas un curso presionando "Agregar al carrito"
+    listaCursos.addEventListener("click", agregarCurso)
+
+}
+
+
+// Funciones
+function agregarCurso(){
+    console.log('presionando en cursos')
+}
---
+++ iteradores/01.app.js
@@ -0,0 +1,19 @@
+// ciclor for o for loop
+
+// for(let i=0; i<10; i++){
+//     let resultado = '';
+//     for(let j=i; j<10; j++){
+//         resultado += ' *';
+//     }
+//     console.log(resultado);
+// }
+
+
+// numeros impares con ciclo for
+for (let i = 0; i <= 20; i++) {
+    if(i % 2 === 0){
+        console.log(`El numero ${i} es par`);
+    }else{
+        console.log(`El numero ${i} es impar`);
+    }
+}
